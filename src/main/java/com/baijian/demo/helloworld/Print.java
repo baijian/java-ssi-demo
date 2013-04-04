0822745e331abf1c3e5940b1d0461cc7dc74ffb5
柏健 <jian.baij@gmail.com>
2013-04-05 03:26:01 +0800

    add print hi

---
+++ src/main/java/com/baijian/demo/helloworld/Print.java
@@ -0,0 +1,13 @@
+package main.java.com.baijian.demo.helloworld;
+
+public class Print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
